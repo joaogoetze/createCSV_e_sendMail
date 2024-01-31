eff3570bc0e145948ae46f955958f854262bb38c
joaogoetze <joaoaugustoferreira5@gmail.com>
2024-01-30 23:10:39 -0300

    Pegando dados do arquivo CSV

--- getProducts.php
+++ getProducts.php
@@ -10,31 +10,18 @@ function readCSV()
 {
     $filename = 'base/products.csv';
     $file = fopen($filename, 'r');
-    $content = fgetcsv($file);
-    print_r($content);
+
+    while(! feof($file))
+    {
+        echo "<br>";
+        print_r(fgetcsv($file)[0]);
+    }
+    fclose($file);
 }
 
-/*
-$row = 1;
-if (($handle = fopen("csv.csv", "r")) !== FALSE)
-{
-    //Passagem pelas linhas
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE)
-    {
-        $num = count($data);
-        $row++;
-        // Passagem pelas colunas
-        for ($col = 0; $col < $num; $col++)
-        {
-            //Printando apenas a coluna 14
-            if ($col == 14)
-            {
-                echo 'Exibindo valor para linha:'.$row.' e para a coluna: '.$data[$col] . "<br />\n";
-            }
-        }
-    }
-    fclose($handle);
-}*/
+
+
+
 
 readCSV();
 
